3a7688b14e45a1808822427d595d8efc4af9bb2d
JaneMercer <5802984@example.com>
2019-06-06 20:11:31 +0300

    -modifided

--- select_inv.py
+++ select_inv.py
@@ -1,14 +1,18 @@
 import cv2  # instal as opencv-python
-#import time
+# import time
 import os
 import numpy as np
 import tensorflow as tf
+import pyautogui.screenshotUtil
 import matplotlib.pyplot as plt
-#import pyautogui.screenshotUtil
+
+# import pyautogui.screenshotUtil
 
 DATADIR = "C:\\Users\\Ira\PycharmProjects\Scp_medkits"
 IMG_SIZE = 60
 model = tf.keras.models.load_model("3 - CNV - 64 - ND - 0 - DNS - 1559109706.model")
+inv_coordArr = [(1080, 325), (1250, 450), (1250, 720), (850, 900), (1080, 900), (660, 730), (660, 470), (830, 300)]
+
 
 def prepare(img):
     IMG_SIZE = 60
@@ -17,24 +21,25 @@ def prepare(img):
 
 
 def run_predict(img_):
-
     try:
         if img_.shape:
-                prediction = model.predict([prepare(img_)])
-                if prediction == 1:
-                    category = "Medkit"
-                else:
-                    category = "Not Medkit"
+            prediction = model.predict([prepare(img_)])
+            if prediction == 1:
+                category = 1
+                # category = "Medkit"
+            else:
+                category = 0
+                # category = "Not Medkit"
 
-                print(category)
-                plt.imshow(img_, cmap="gray")
-                plt.show()
+            print(category)
+            # plt.imshow(img_, cmap="gray")
+            # plt.show()
         else:
-                print("Dos not exist")
+            print("Dos not exist")
 
     except Exception as e:
-            print(e)
-            pass
+        print(e)
+        pass
     return category
 
 
@@ -49,7 +54,8 @@ def select(mat, point0, point1):
 
 
 def my_crop(img_mask, img):
-    contours, _ = cv2.findContours(img_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # ХРОБИ ТАК ШОБ ЗНАХОДИЛОКВАДРАТ
+    contours, _ = cv2.findContours(img_mask, cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_SIMPLE)  # ХРОБИ ТАК ШОБ ЗНАХОДИЛОКВАДРАТ
     cnt = contours[0]
     rect = cv2.minAreaRect(cnt)
     box = cv2.boxPoints(rect)
@@ -59,38 +65,43 @@ def my_crop(img_mask, img):
 
 def select_mask(img):
     path_mask = os.path.join(DATADIR, "MSC_SQAURE")
-    img_indx = []
-    try:
-        scaled_v_img = img[180:1030, 530:1385]
-        for img_m in os.listdir(path_mask):
-            try:
-                    img_mask = cv2.imread(os.path.join(path_mask, img_m), cv2.IMREAD_GRAYSCALE)
-                    selected_img = cv2.bitwise_and(scaled_v_img, scaled_v_img, mask=img_mask)
-                    res = my_crop(img_mask, selected_img)
+    scaled_v_img = img[180:1030, 530:1385]
 
-                    height_к, width_к = res.shape[:2]
-                    if height_к < 50 or width_к < 50:
-                        plt.imshow(res, cmap="gray")
-                        plt.show()
-                    else:
-                        scaled_res = cv2.resize(res, (IMG_SIZE, IMG_SIZE))
+    for indx, img_m in enumerate(os.listdir(path_mask)):
+        try:
+            img_mask = cv2.imread(os.path.join(path_mask, img_m), cv2.IMREAD_GRAYSCALE)
+            selected_img = cv2.bitwise_and(scaled_v_img, scaled_v_img, mask=img_mask)
+            res = my_crop(img_mask, selected_img)
 
-                        predict_res = run_predict(scaled_res) #add import p-Predict
-                        #img_indx.append((img_m, predict_res))
-                        if predict_res == 1:
-                            return img_m
-            except Exception as e:
-                    print(e)
-                    pass
+            height_к, width_к = res.shape[:2]
+            if height_к >= IMG_SIZE or width_к >= IMG_SIZE:
+                scaled_res = cv2.resize(res, (IMG_SIZE, IMG_SIZE))
 
-    except Exception as e:
-        print(e)
-        pass
+                predict_res = run_predict(scaled_res)  # add import p-Predict
+                # img_indx.append((img_m, predict_res))
+                if predict_res == 1:
+                    return indx
+        except Exception as e:
+            print(e)
+            pass
+    return 0
+
+
+def click_inv(index_inv):
+    x, y = inv_coordArr[index_inv]
+    pyautogui.moveTo(x, y, duration=0.1)
 
 
 scrn = cv2.imread('for-testing.png', 0)
 
-select_mask(scrn)
+index_inv = select_mask(scrn)
 
+print('__________')
+print(index_inv)
 
-#scrn = pyautogui.screenshot()
+if index_inv == 0:
+    print("No medkit")
+else:
+    click_inv(index_inv)
+
+# scrn = pyautogui.screenshot()
